474ff1c7f57f65685a35a9d6dd3537e0c8d4817b
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-02 15:18:49 -0400

    add javascript to button to animate it when clicked

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("its working!!!");
+const hamburger = document.querySelector(".hamburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+
+hamburger.addEventListener("click", () => {
+  bar1.classList.toggle("animatebar1");
+  bar2.classList.toggle("animatebar2");
+  bar3.classList.toggle("animatebar3");
+});
