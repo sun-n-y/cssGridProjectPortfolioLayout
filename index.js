df3426fea871f5f4b8201113caca14e0a52b5957
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-02 15:45:08 -0400

    add toggle feature

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const hamburger = document.querySelector(".hamburger");
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
+const mobileNav = document.querySelector(".mobileNav");
 
 hamburger.addEventListener("click", () => {
   bar1.classList.toggle("animatebar1");
   bar2.classList.toggle("animatebar2");
   bar3.classList.toggle("animatebar3");
+  mobileNav.classList.toggle("openDrawer");
 });
